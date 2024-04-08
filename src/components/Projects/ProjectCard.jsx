8fe0eb91a1fb3fe8eb67588d43ca97e3fe301186
Thai Huynh <rafehuynh0510@gmail.com>
2024-04-08 18:50:58 -0500

    added animations to each components

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { getImageUrl } from '../../ultis'
 import styles from "./ProjectCard.module.css"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 const ProjectCard = ({item}) => {
+  AOS.init({
+    easing: 'ease-out-quart',
+    delay: 0,
+    duration: 1000
+  })
   return (
-    <div className={styles.container}>
+    <div className={styles.container} data-aos="flip-up">
         <img src={getImageUrl(item.imageSrc)} alt ={item.title} className={styles.image}/>
         <h3 className={styles.title}>{item.title}</h3>
         <p className={styles.desc}>{item.desc}</p>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,7 +4,14 @@ import experience from "../../../data/experience.json"
 import { getImageUrl } from '../../ultis'
 import styles from "./Skills.module.css"
 import { CiCalendarDate } from "react-icons/ci";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 const Skills = () => {
+    AOS.init({
+        easing: 'ease-out-quart',
+        delay: 0,
+        duration: 1000
+      }) 
   return (
     <section id="skills"className={styles.container}>
         <h2 className={styles.title}>
@@ -14,7 +21,7 @@ const Skills = () => {
             <div className={styles.skills}>
                 {
                     skills.map((skill, id) => {
-                      return <div key={id} className={styles.skill}>
+                      return <div key={id} className={styles.skill} data-aos="fade-up-right">
                         <div className={styles.skillImage}><img src={getImageUrl(skill.imageSrc)} alt={skill.title}/></div>
                         <p>{skill.title}</p>
                         </div>  
@@ -24,7 +31,7 @@ const Skills = () => {
             <ul className={styles.experience}>
                 {
                     experience.map((item, id) => {
-                        return <li key={id} className={styles.experienceItem}>
+                        return <li key={id} className={styles.experienceItem} data-aos="fade-up-left">
                             <div className={styles.experienceDetails}>
                                 <h3>{`${item.role}, ${item.company}`}</h3>
                                 <p><CiCalendarDate size={20}/> {`${item.startDate} - ${item.endDate}`}</p>
